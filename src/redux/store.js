83b245be88e287a34d3ad993122e7dbafa90c5a6
Alexander Klymuk <swcAlexander@gmail.com>
2023-08-04 08:00:05 +0300

    add start API

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import fetchGallery from 'Api/ApiServise';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import rootReducer from 'redux/reducer';
@@ -13,6 +14,7 @@ const costomMiddle = state => {
   return next => {
     return action => {
       if (typeof action === 'function') {
+        const contactsList = fetchGallery();
         return next(action(state));
       }
       return next(action);
